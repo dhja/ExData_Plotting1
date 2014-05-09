e000bc7f0de099edd12e6155618c12a1790ca0e9
dhja <dhirenjani@users.noreply.github.com>
2014-05-09 23:42:02 +0530

    final attempt

--- plot3.R
+++ plot3.R
@@ -8,9 +8,10 @@
   power4$Sub_metering_1 <- as.numeric(power4$Sub_metering_1)
   power4$Sub_metering_2 <- as.numeric(power4$Sub_metering_2)
   power4$Sub_metering_3 <- as.numeric(power4$Sub_metering_3)
-
+  #draw the plot
+  png(filename = "plot3.png", width = 480, height = 480)
   plot(power4$datetime,power4$Sub_metering_1,type="l",ylab="Energy sub metering",xlab="")
   points(power4$datetime,power4$Sub_metering_2,type="l",col="red")
   points(power4$datetime,power4$Sub_metering_3,type="l",col="blue")
-  legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1,1),lwd=c(2.5,2.5,2.5),col=c("black","red","blue"))
-
+  legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1,1),lwd=c(2.5,2.5,2.5),col=c("black","red","blue"),bty="n")
+  dev.off()
--- plot4.R
+++ plot4.R
@@ -11,6 +11,8 @@ power4$Sub_metering_1 <- as.numeric(power4$Sub_metering_1)
 power4$Sub_metering_2 <- as.numeric(power4$Sub_metering_2)
 power4$Sub_metering_3 <- as.numeric(power4$Sub_metering_3)
 
+#draw the plot
+png(filename = "plot4.png", width = 480, height = 480)
 par(mfrow=c(2,2))
 plot(power4$datetime,power4$Global_active_power,type="l",ylab="Global Active Power",xlab="")
 
@@ -19,8 +21,8 @@ plot(power4$datetime,power4$Voltage,type="l",ylab="Voltage",xlab="datetime")
 plot(power4$datetime,power4$Sub_metering_1,type="l",ylab="Energy sub metering",xlab="")
 points(power4$datetime,power4$Sub_metering_2,type="l",col="red")
 points(power4$datetime,power4$Sub_metering_3,type="l",col="blue")
-legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1,1),lwd=c(2.5,2.5,2.5),col=c("black","red","blue"))
+legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1,1),lwd=c(2.5,2.5,2.5),col=c("black","red","blue"),bty="n")
 
-plot(power4$datetime,power4$Global_active_power,type="l",ylab="Global_reactive_power",xlab="datetime")
-
+plot(power4$datetime,power4$Global_reactive_power,type="l",ylab="Global_reactive_power",xlab="datetime")
+dev.off()
 
